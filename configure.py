dd5636ec4c25626a08c29c4c9272db5b73221bd6
Piotr Sarna <sarna@scylladb.com>
2018-10-03 15:03:34 +0200

    table: init table.cc file
    This file will be used to move table-related functions to it.

--- configure.py
+++ configure.py
@@ -431,6 +431,7 @@ extra_cxxflags = {}
 cassandra_interface = Thrift(source='interface/cassandra.thrift', service='Cassandra')
 
 scylla_core = (['database.cc',
+                'table.cc',
                 'atomic_cell.cc',
                 'schema.cc',
                 'frozen_schema.cc',
