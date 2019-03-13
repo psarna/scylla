5dfad5412501a6f0bb450fda824e90f96fb87c43
Piotr Sarna <sarna@scylladb.com>
2019-03-13 13:00:00 +0100

    schema: add computed info to column definition
    Some columns may represent not user-provided values, but ones computed
    from other columns. Currently an example is token column used in secondary
    indexes to provide proper ordering. In order to avoid hardcoding special
    cases in execution stage, optional additional information for computed
    columns is stored in column definition.

---
+++ column_computation.hh
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2019 ScyllaDB
+ */
+
+/*
+ * This file is part of Scylla.
+ *
+ * Scylla is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Scylla is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Scylla.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "bytes.hh"
+
+class schema;
+class partition_key;
+class clustering_row;
+
+class column_computation;
+using column_computation_ptr = std::unique_ptr<column_computation>;
+
+class column_computation {
+public:
+    virtual ~column_computation() = default;
+
+    static column_computation_ptr deserialize(bytes raw);
+
+    virtual column_computation_ptr clone() const = 0;
+
+    virtual bytes serialize() const = 0;
+    virtual bytes_opt compute_value(const schema& schema, const partition_key& key, const clustering_row& row) const = 0;
+};
