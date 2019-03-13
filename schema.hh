9dcbfdd5dd1a094f1765b567791cc1b578de19fb
Piotr Sarna <sarna@scylladb.com>
2019-03-13 13:00:00 +0100

    schema: add computed info to column definition
    Some columns may represent not user-provided values, but ones computed
    from other columns. Currently an example is token column used in secondary
    indexes to provide proper ordering. In order to avoid hardcoding special
    cases in execution stage, optional additional information for computed
    columns is stored in column definition.

--- schema.hh
+++ schema.hh
@@ -38,6 +38,7 @@
 #include "compress.hh"
 #include "compaction_strategy.hh"
 #include "caching_options.hh"
+#include "column_computation.hh"
 
 using column_count_type = uint32_t;
 
@@ -216,6 +217,7 @@ private:
     bool _is_atomic;
     bool _is_counter;
     column_view_virtual _is_view_virtual;
+    column_computation_ptr _computation;
 
     struct thrift_bits {
         thrift_bits()
@@ -231,6 +233,7 @@ public:
     column_definition(bytes name, data_type type, column_kind kind,
         column_id component_index = 0,
         column_view_virtual view_virtual = column_view_virtual::no,
+        column_computation_ptr = nullptr,
         api::timestamp_type dropped_at = api::missing_timestamp);
 
     data_type type;
@@ -242,6 +245,36 @@ public:
 
     column_kind kind;
     ::shared_ptr<cql3::column_specification> column_specification;
+
+
+    column_definition(const column_definition& other)
+            : _name(other._name)
+            , _dropped_at(other._dropped_at)
+            , _is_atomic(other._is_atomic)
+            , _is_counter(other._is_counter)
+            , _is_view_virtual(other._is_view_virtual)
+            , _computation(other.get_computation_ptr())
+            , _thrift_bits(other._thrift_bits)
+            , type(other.type)
+            , id(other.id)
+            , kind(other.kind)
+            , column_specification(other.column_specification)
+        {}
+
+    column_definition& operator=(const column_definition& other) {
+        _name = other._name;
+        _dropped_at = other._dropped_at;
+        _is_atomic = other._is_atomic;
+        _is_counter = other._is_counter;
+        _is_view_virtual = other._is_view_virtual;
+        _computation = other.get_computation_ptr();
+        _thrift_bits = other._thrift_bits;
+        type = other.type;
+        id = other.id;
+        kind = other.kind;
+        column_specification = other.column_specification;
+        return *this;
+    }
 
     bool is_static() const { return kind == column_kind::static_column; }
     bool is_regular() const { return kind == column_kind::regular_column; }
@@ -257,6 +290,14 @@ public:
     // These columns should be hidden from the user's SELECT queries.
     bool is_view_virtual() const { return _is_view_virtual == column_view_virtual::yes; }
     column_view_virtual view_virtual() const { return _is_view_virtual; }
+    // Computed column values are generated from other columns instead of storing values
+    // provided by the user.
+    bool is_computed() const { return bool(_computation); }
+    const column_computation& get_computation() const { return *_computation; }
+    column_computation_ptr get_computation_ptr() const {
+        return _computation ? _computation->clone() : nullptr;
+    }
+    void set_computed(column_computation_ptr computation) { _computation = std::move(computation); }
     // Columns hidden from CQL cannot be in any way retrieved by the user,
     // either explicitly or via the '*' operator, or functions, aggregates, etc.
     bool is_hidden_from_cql() const { return is_view_virtual(); }
