e7173a99b3389eafa2e767e7c205f2148106dc4b
Piotr Sarna <sarna@scylladb.com>
2019-04-04 15:22:54 +0200

    index_metadata: reduce the number of target parsing
    Parsing a target from JSON more than one is detrimental to performance.
    In order to reduce recomputations, parsed target info is cached
    inside index metadata.

--- schema.cc
+++ schema.cc
@@ -500,6 +500,18 @@ bool index_metadata::local() const {
     return _local;
 }
 
+const secondary_index::target_info& index_metadata::get_target_info(const schema& schema) const {
+    if (!_parsed_target) {
+        _parsed_target = ::make_shared<secondary_index::target_info>(secondary_index::target_parser::parse(schema.shared_from_this(), *this));
+    }
+    return *_parsed_target;
+}
+
+const column_definition* index_metadata::get_target_column(const schema& schema) const {
+    const auto& target_info = get_target_info(schema);
+    return local() ? target_info.ck_columns.front() : target_info.pk_columns.front();
+}
+
 sstring index_metadata::get_default_index_name(const sstring& cf_name,
                                                std::optional<sstring> root) {
     if (root) {
