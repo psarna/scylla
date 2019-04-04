cc798cbe44e89df49d9ae4bcbbac8ac9c4faeb04
Piotr Sarna <sarna@scylladb.com>
2019-04-04 14:12:03 +0200

    row_marker: move to separate header file
    It will be needed to avoid circular dependencies in future commits.

---
+++ row_marker.hh
@@ -0,0 +1,130 @@
+/*
+ * Copyright (C) 2019 ScyllaDB
+ */
+
+/*
+ * This file is part of Scylla.
+ *
+ * Scylla is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Scylla is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Scylla.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "gc_clock.hh"
+#include "timestamp.hh"
+#include "tombstone.hh"
+
+class compaction_garbage_collector;
+
+class row_marker;
+int compare_row_marker_for_merge(const row_marker& left, const row_marker& right) noexcept;
+
+class row_marker {
+    static constexpr gc_clock::duration no_ttl { 0 };
+    static constexpr gc_clock::duration dead { -1 };
+    static constexpr gc_clock::time_point no_expiry { gc_clock::duration(0) };
+    api::timestamp_type _timestamp = api::missing_timestamp;
+    gc_clock::duration _ttl = no_ttl;
+    gc_clock::time_point _expiry = no_expiry;
+public:
+    row_marker() = default;
+    explicit row_marker(api::timestamp_type created_at) : _timestamp(created_at) { }
+    row_marker(api::timestamp_type created_at, gc_clock::duration ttl, gc_clock::time_point expiry)
+        : _timestamp(created_at), _ttl(ttl), _expiry(expiry)
+    { }
+    explicit row_marker(tombstone deleted_at)
+        : _timestamp(deleted_at.timestamp), _ttl(dead), _expiry(deleted_at.deletion_time)
+    { }
+    bool is_missing() const {
+        return _timestamp == api::missing_timestamp;
+    }
+    bool is_live() const {
+        return !is_missing() && _ttl != dead;
+    }
+    bool is_live(tombstone t, gc_clock::time_point now) const {
+        if (is_missing() || _ttl == dead) {
+            return false;
+        }
+        if (_ttl != no_ttl && _expiry <= now) {
+            return false;
+        }
+        return _timestamp > t.timestamp;
+    }
+    // Can be called only when !is_missing().
+    bool is_dead(gc_clock::time_point now) const {
+        if (_ttl == dead) {
+            return true;
+        }
+        return _ttl != no_ttl && _expiry <= now;
+    }
+    // Can be called only when is_live().
+    bool is_expiring() const {
+        return _ttl != no_ttl;
+    }
+    // Can be called only when is_expiring().
+    gc_clock::duration ttl() const {
+        return _ttl;
+    }
+    // Can be called only when is_expiring().
+    gc_clock::time_point expiry() const {
+        return _expiry;
+    }
+    // Should be called when is_dead() or is_expiring().
+    // Safe to be called when is_missing().
+    // When is_expiring(), returns the the deletion time of the marker when it finally expires.
+    gc_clock::time_point deletion_time() const {
+        return _ttl == dead ? _expiry : _expiry - _ttl;
+    }
+    api::timestamp_type timestamp() const {
+        return _timestamp;
+    }
+    void apply(const row_marker& rm) {
+        if (compare_row_marker_for_merge(*this, rm) < 0) {
+            *this = rm;
+        }
+    }
+    // Expires cells and tombstones. Removes items covered by higher level
+    // tombstones.
+    // Returns true if row marker is live.
+    bool compact_and_expire(tombstone tomb, gc_clock::time_point now,
+            can_gc_fn& can_gc, gc_clock::time_point gc_before, compaction_garbage_collector* collector = nullptr);
+    // Consistent with feed_hash()
+    bool operator==(const row_marker& other) const {
+        if (_timestamp != other._timestamp) {
+            return false;
+        }
+        if (is_missing()) {
+            return true;
+        }
+        if (_ttl != other._ttl) {
+            return false;
+        }
+        return _ttl == no_ttl || _expiry == other._expiry;
+    }
+    bool operator!=(const row_marker& other) const {
+        return !(*this == other);
+    }
+    // Consistent with operator==()
+    template<typename Hasher>
+    void feed_hash(Hasher& h) const {
+        ::feed_hash(h, _timestamp);
+        if (!is_missing()) {
+            ::feed_hash(h, _ttl);
+            if (_ttl != no_ttl) {
+                ::feed_hash(h, _expiry);
+            }
+        }
+    }
+    friend std::ostream& operator<<(std::ostream& os, const row_marker& rm);
+};
