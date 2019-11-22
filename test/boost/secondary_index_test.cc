28ab484190166d7bac15df6208da32f4bb58ad12
Piotr Sarna <sarna@scylladb.com>
2019-11-22 10:14:23 +0100

    tests: add ghost row deletion test case
    The tests checks if manually injected ghost rows are properly deleted
    by the ghost row delete statement - and, that non-ghost regular rows
    are left intact.

--- test/boost/secondary_index_test.cc
+++ test/boost/secondary_index_test.cc
@@ -30,6 +30,7 @@
 #include "test/lib/exception_utils.hh"
 #include "cql3/statements/select_statement.hh"
 
+using namespace std::chrono_literals;
 
 SEASTAR_TEST_CASE(test_secondary_index_regular_column_query) {
     return do_with_cql_env([] (cql_test_env& e) {
@@ -1237,6 +1238,93 @@ SEASTAR_TEST_CASE(test_filtering_indexed_column) {
             auto msg = cquery_nofail(e, "select d from ks.test_index where c > 25 ALLOW FILTERING;");
             assert_that(msg).is_rows().with_rows({
                 {int32_type->decompose(44), int32_type->decompose(33)}
+            });
+        });
+    });
+}
+
+SEASTAR_TEST_CASE(test_deleting_ghost_rows) {
+    return do_with_cql_env_thread([] (auto& e) {
+        cquery_nofail(e, "CREATE TABLE t (p int, c int, v int, PRIMARY KEY (p, c))");
+        cquery_nofail(e, "CREATE MATERIALIZED VIEW tv AS SELECT v, p, c FROM t WHERE v IS NOT NULL AND c IS NOT NULL PRIMARY KEY (v, p, c);");
+        cquery_nofail(e, "INSERT INTO t (p,c,v) VALUES (1,1,1)");
+        cquery_nofail(e, "INSERT INTO t (p,c,v) VALUES (1,2,3)");
+        cquery_nofail(e, "INSERT INTO t (p,c,v) VALUES (2,4,6)");
+
+        auto inject_ghost_row = [&e] (int pk) {
+            e.db().invoke_on_all([pk] (database& db) {
+                schema_ptr schema = db.find_schema("ks", "tv");
+                table& t = db.find_column_family(schema);
+                mutation m(schema, partition_key::from_singular(*schema, pk));
+                auto& row = m.partition().clustered_row(*schema, clustering_key::from_exploded(*schema, {int32_type->decompose(8), int32_type->decompose(7)}));
+                row.apply(row_marker{api::new_timestamp()});
+                unsigned shard = db.shard_of(m);
+                if (shard == engine().cpu_id()) {
+                    t.apply(m);
+                }
+            }).get();
+        };
+
+        inject_ghost_row(9);
+        eventually([&] {
+            // The ghost row exists, but it can only be queried from the view, not from the base
+            auto msg = cquery_nofail(e, "SELECT * FROM tv WHERE v = 9;");
+            assert_that(msg).is_rows().with_rows({
+                {int32_type->decompose(9), int32_type->decompose(8), int32_type->decompose(7)},
+            });
+        });
+
+        // Ghost row deletion is attempted for a single view partition
+        cquery_nofail(e, "DELETE IF NOT EXISTS FROM MATERIALIZED VIEW tv WHERE v = 9");
+        eventually([&] {
+            // The ghost row is deleted
+            auto msg = cquery_nofail(e, "SELECT * FROM tv where v = 9;");
+            assert_that(msg).is_rows().with_size(0);
+        });
+
+        for (int i = 0; i < 4321; ++i) {
+            inject_ghost_row(10 + i);
+        }
+        eventually([&] {
+            auto msg = cquery_nofail(e, "SELECT * FROM tv;");
+            assert_that(msg).is_rows().with_size(4321 + 3);
+        });
+
+        // Ghost row deletion is attempted for the whole table
+        cquery_nofail(e, "DELETE IF NOT EXISTS FROM MATERIALIZED VIEW tv;");
+        eventually([&] {
+            // Ghost rows are deleted
+            auto msg = cquery_nofail(e, "SELECT * FROM tv;");
+            assert_that(msg).is_rows().with_rows_ignore_order({
+                {int32_type->decompose(1), int32_type->decompose(1), int32_type->decompose(1)},
+                {int32_type->decompose(3), int32_type->decompose(1), int32_type->decompose(2)},
+                {int32_type->decompose(6), int32_type->decompose(2), int32_type->decompose(4)}
+            });
+        });
+
+        for (int i = 0; i < 2345; ++i) {
+            inject_ghost_row(10 + i);
+        }
+        eventually([&] {
+            auto msg = cquery_nofail(e, "SELECT * FROM tv;");
+            assert_that(msg).is_rows().with_size(2345 + 3);
+        });
+
+        // Ghost row deletion is attempted with a parallelized table scan
+        when_all(
+            e.execute_cql("DELETE IF NOT EXISTS FROM MATERIALIZED VIEW tv WHERE token(v) >= -9223372036854775807 AND token(v) <= 0"),
+            e.execute_cql("DELETE IF NOT EXISTS FROM MATERIALIZED VIEW tv WHERE token(v) > 0 AND token(v) <= 10000000"),
+            e.execute_cql("DELETE IF NOT EXISTS FROM MATERIALIZED VIEW tv WHERE token(v) > 10000000 AND token(v) <= 20000000"),
+            e.execute_cql("DELETE IF NOT EXISTS FROM MATERIALIZED VIEW tv WHERE token(v) > 20000000 AND token(v) <= 30000000"),
+            e.execute_cql("DELETE IF NOT EXISTS FROM MATERIALIZED VIEW tv WHERE token(v) > 30000000 AND token(v) <= 9223372036854775807")
+        ).get();
+        eventually([&] {
+            // Ghost rows are deleted
+            auto msg = cquery_nofail(e, "SELECT * FROM tv;");
+            assert_that(msg).is_rows().with_rows_ignore_order({
+                {int32_type->decompose(1), int32_type->decompose(1), int32_type->decompose(1)},
+                {int32_type->decompose(3), int32_type->decompose(1), int32_type->decompose(2)},
+                {int32_type->decompose(6), int32_type->decompose(2), int32_type->decompose(4)}
             });
         });
     });
