e73eaf58be24bef6117fe773df7cd7e368f0228c
Botond Dénes <bdenes@scylladb.com>
2020-05-18 11:25:22 +0300

    reader_permit: resource_units: work in terms of reader_resources
    Refactor resource_units semantically as well to work in terms of
    reader_resources, instead of just memory.

--- reader_concurrency_semaphore.hh
+++ reader_concurrency_semaphore.hh
@@ -128,15 +128,12 @@ private:
         return has_available_units(r) && _wait_list.empty();
     }
 
-    void consume_memory(size_t memory) {
-        _resources.memory -= memory;
+    void consume(const resources& r) {
+        _resources -= r;
     }
 
     void signal(const resources& r) noexcept;
 
-    void signal_memory(size_t memory) noexcept {
-        signal(resources(0, static_cast<ssize_t>(memory)));
-    }
 public:
     struct no_limits { };
 
