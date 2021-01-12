8fc82cfd467ec55a86a90f40557f58456617a5b8
Piotr Sarna <sarna@scylladb.com>
2021-01-12 10:46:00 +0100

    fixup special delete marker

--- cql3/statements/sl_prop_defs.cc
+++ cql3/statements/sl_prop_defs.cc
@@ -23,7 +23,7 @@ void sl_prop_defs::validate() {
             return std::nullopt;
         }
         if (boost::algorithm::iequals(*repr, "null")) {
-            return lowres_clock::duration::max();
+            return qos::service_level_options::delete_marker;
         }
         data_value v = duration_type->deserialize(duration_type->from_string(*repr));
         cql_duration duration = static_pointer_cast<const duration_type_impl>(duration_type)->from_value(v);
@@ -33,7 +33,11 @@ void sl_prop_defs::validate() {
         if (duration.nanoseconds % 1'000'000 != 0) {
             throw exceptions::invalid_request_exception("Timeout values must be expressed in millisecond granularity");
         }
-        return std::make_optional(std::chrono::duration_cast<lowres_clock::duration>(std::chrono::nanoseconds(duration.nanoseconds)));
+        auto ret = std::chrono::duration_cast<lowres_clock::duration>(std::chrono::nanoseconds(duration.nanoseconds));
+        if (ret == qos::service_level_options::delete_marker) {
+            throw exceptions::invalid_request_exception(format("Timeout value {}ns is reserved and cannot be explicitly used", duration.nanoseconds));
+        }
+        return ret;
     };
 
     property_definitions::validate(timeout_props);
